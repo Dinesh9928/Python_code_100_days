c188dc81dcacfaf80b45199bbc726568f7dbcb7d
Dinesh Prasad <88045947+Dinesh9928@users.noreply.github.com>
2024-07-31 06:17:00 +0530

    Update main.py added more function and comments

--- 13-Day13-String-Methods/main.py
+++ 13-Day13-String-Methods/main.py
@@ -1,58 +1,67 @@
 # Strings are immutable
-a = "!!!Harry!! !!!!!!!!! Harry"
-print(len(a))
-print(a)
-print(a.upper())
-print(a.lower())
-print(a.rstrip("!"))
-print(a.lstrip("!"))
-print(a.strip("!"))
-print(a.replace("Harry", "John"))
-print(a.split(" "))
-blogHeading = "introduction tO jS"
-print(blogHeading.capitalize())
+# a = "!!!Harry!! !!!!!!!!! Harry !!!!!"
+# print(a)
 
-str1 = "Welcome to the Console!!!"
-print(len(str1))
-print(len(str1.center(50)))
-print(a.count("Harry"))
+# print(len(a))
+# print(a)
+# print(a.upper())
+# print(a.lower())
+# print(a.rstrip("!"))
+# print(a.lstrip("!"))
+# print(a.strip("!"))
+# print(a.replace("Harry", "John"))
+# print(a.split(" "))
+# blogHeading = "introduction tO jS"
+# print(blogHeading.capitalize())
 
-str1 = "Welcome to the Console !!!"
-print(str1.endswith("!!!"))
+# str1 = "Welcome to the Console!!!"
+# print(len(str1))
+# print(str1)
+# print(str1.center(50))
+# print(len(str1.center(50)))
+# print(a.count("Harry"))
+# print(a.count("!"))
 
-str1 = "Welcome to the Console !!!"
-print(str1.endswith("to", 4, 10))
+# str1 = "Welcome to the Console !!!"
+# print(str1.endswith("!!!"))
 
-str1 = "He's name is Dan. He is an honest man."
-print(str1.find("ishh"))
-# print(str1.index("ishh"))
+# str1 = "Welcome to the Console !!!"
+# print(str1.endswith("to", 4, 10))
+# print(str1.endswith("to", 4, 9))
 
-str1 = "WelcomeToTheConsole"
-print(str1.isalnum())
-str1 = "Welcome"
-print(str1.isalpha())
+# str1 = "He's name is Dan. He is an honest man."
+# print(str1.find("isjj"))
+# print(str1.find("He's"))
+# print(str1.index("isjj")) # gives error if substring not found
+# print(str1.index("He's"))
 
-str1 = "hello world"
-print(str1.islower())
+# str1 = "WelcomeToTheConsole12"
+# print(str1.isalnum()) # alpha-numeric
+# print(str1.isalpha()) # only alpha char
 
-str1 = "We wish you a Merry Christmas\n"
-print(str1.isprintable())
-str1 = "         "       #using Spacebar
-print(str1.isspace())
-str2 = "  "       #using Tab
-print(str2.isspace())
+# str1 = "hello world"
+# print(str1.islower())
 
-str1 = "World Health Organization" 
-print(str1.istitle())
+# str1 = "    We  wish you a Merry Christmas\n    "
+# str1 = "    We  wish you a Merry Christmas   "
+# # print(str1.isprintable()) # can be printed to consol or not 
+# str1 = "      "       #using Spacebar all the char should be white space. 
+# print(str1.isspace())
+# str2 = "  "       #using Tab both same
+# print(str2.isspace())
 
-str2 = "To kill a Mocking bird"
-print(str2.istitle())
+# str1 = "World Health Organization" 
+# print(str1.istitle())
 
-str1 = "Python is a Interpreted Language" 
-print(str1.startswith("Python"))
+# str2 = "To kill a Mocking bird"
+# str2 = "To Kill A Mocking Bird"
+# print(str2.istitle())
 
-str1 = "Python is a Interpreted Language" 
-print(str1.swapcase())
+# str1 = "Python is a Interpreted Language" 
+# print(str1.startswith("Python"))
 
-str1 = "His name is Dan. Dan is an honest man."
-print(str1.title())
+# str1 = "Python is a Interpreted Language" 
+# print(str1.swapcase())
+
+# str1 = "His name is Dan. Dan is an honest man."
+# print(str1.title())
