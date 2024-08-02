b424e56c0408c5fe743649707e081af8dcd46403
Dinesh Prasad <88045947+Dinesh9928@users.noreply.github.com>
2024-08-02 06:56:36 +0530

    Update main.py

--- 24-Day24-Introduction-to-Tuples/main.py
+++ 24-Day24-Introduction-to-Tuples/main.py
@@ -1,13 +1,16 @@
 tup = (1, 2, 76, 342, 32, "green", True)
-# tup[0] = 90
+tup[0] = 90 # typeError 
 print(type(tup), tup)
 print(len(tup))
 print(tup[0])
 print(tup[-1])
 print(tup[2])
-# print(tup[34])
+print(tup[34]) #indexError
 
-if  3421 in tup:
+if  342 in tup:
   print("Yes 342 is present in this tuple")
+else:
+  print("no not there")
+  
 tup2 = tup[1:4]
 print(tup2)
