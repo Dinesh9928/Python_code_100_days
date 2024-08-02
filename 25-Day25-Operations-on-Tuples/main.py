a065e01f6d7f71c69eae735504ce1f412f4beac7
Dinesh Prasad <88045947+Dinesh9928@users.noreply.github.com>
2024-08-02 07:20:03 +0530

    Update main.py

--- 25-Day25-Operations-on-Tuples/main.py
+++ 25-Day25-Operations-on-Tuples/main.py
@@ -1,7 +1,14 @@
 tuple1 = (0, 1, 2, 3, 2, 31, 1, 3, 2, 3)
-# res = tuple1.count(3)
-# res = tuple1.index(3)
-# res = tuple1.index(311)
-# res = tuple1.index(3, 4, 8)
+res = tuple1.count(3)
+
+ind1 = tuple1.index(3)
+print(ind1)
+
+ind2 = tuple1.index(3, 4, 8) # start = 4 end = 8 index range
+print(ind2)
+
+res = tuple1.index(311) # valueError 
+res = tuple1.index(3, 4, 8)
 res = len(tuple1)
 print('Count of 3 in tuple1 is:', res)
+print(res)
