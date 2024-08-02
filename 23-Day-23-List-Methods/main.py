875faef510e91357eca49f6cd75250de51c8387b
Dinesh Prasad <88045947+Dinesh9928@users.noreply.github.com>
2024-08-02 06:38:51 +0530

    Update main.py added more functions

--- 23-Day-23-List-Methods/main.py
+++ 23-Day-23-List-Methods/main.py
@@ -1,15 +1,23 @@
 l = [11, 45, 1, 2, 4, 6, 1, 1]
+
+l.append(7)
 print(l)
-# l.append(7)
-# l.sort(reverse=True)
-# l.reverse()
-# print(l.index(1))
-# print(l.count(1))
-# m = l.copy()
-# m[0] = 0
-# l.insert(1, 899)
+l.sort(reverse=True)
+l.sort()
+l.reverse()
+print(l.index(1))
+print(l.index(3)) # gives valueError when ele present
+print(l.count(1))
+
+m = l.copy()
+m[0] = 0  # 0 index ele removed
+l.insert(0, 0) # the ele not removed, insert at index by adjusting exsisting value. 
+# print(l)
+# print(m)
+l.insert(1, 899)
+
 m = [900, 1000, 1100]
-k = l + m
+# k = m + l
 # print(k)
-# l.extend(m)
+l.extend(m) # l = l + m 
 print(l)
